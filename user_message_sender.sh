4ced0742407f6712f74f95c294662e8ebfc2e964
tomarovsky <andrey.tomarovsky@gmail.com>
2021-05-25 13:31:31 +0300

    fix

---
+++ user_message_sender.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+useroverfile=''
+date_now=$(date +%Y%m%d)
+dryrun='false'
+
+print_usage() {
+	echo "send messages to users"
+	echo "Usage: '-i' your users_over CSV file"
+	echo "-d flag for dryrun"
+}
+
+while getopts 'i:d' flag; do
+	case "${flag}" in
+		i) useroverfile="${OPTARG}" ;;
+		d) dryrun='true' ;;
+		*) print_usage
+			exit 1 ;;
+	esac
+done
+
+AOZmail="aozalevsky@gmail.com"
+dryrun_output="user_messages_${date_now}.txt"
+
+# script
+cat ${useroverfile} | while read line; do
+	username=`echo $line | awk '{print $1}'` ;
+	usermail=`getent passwd | grep "^${username}:" | awk -F ':' '{print $5}'` ;
+	lastdata=`echo $line | awk '{print $2,$3,$4,$5,$6,$7}'`;
+	usedspace=`echo $line | awk '{print $NF}'` ;
+	message="Добрый день, ${username}.\n
+	Судя по логам, вы не логинились на кластер более трех месяцев (${lastdata}) при этом сейчас у нас хранится ~${usedspace} ваших данных. Я хотел бы уточнить их актуальность и необходимость в сохранении вашего аккаунта. В случае отсутствия ответа в течение недели, данные автоматически будут перемещены в архив.\n
+	С уважением,
+	администраторы кластера" ;	
+	if ${dryrun}; then
+		echo -e "---- message to ${username} ----" ;
+		echo -e ${message} | tee -a ${dryrun_output} ;
+	else
+		echo -e ${message} | mail -s "Аккаунт на кластере 'Макарьич'" "${usermail},${AOZmail}" ;
+	fi
+done
---
+++ user_overtimer_grep.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e -u
+
+timeover=90
+without_blocked_users=true
+without_sbinnologin_users=true
+
+print_usage() {
+        echo "get sorted CSV file with users and occupied space"
+        echo "Usage: '-l' your last login time over (default 90)"
+	echo "-b flag to save blocked users to an outfile"
+	echo "-n flag to save users with /sbin/nologin status"
+}
+
+while getopts 'l:bn' flag; do
+        case "${flag}" in
+                l) timeover="${OPTARG}" ;;
+		b) without_blocked_users=false ;;
+		n) without_sbinnologin_users=false ;;
+                *) print_usage
+                        exit 1 ;;
+        esac
+done
+
+date_now=$(date +%Y%m%d)
+output=users_over${timeover}_${date_now}.csv
+
+# script
+# get sort CSV file with users, dates and occupied space
+ssh head02 lastlog -b ${timeover} |\
+awk '!/Never log/ {printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t",$1,$4,$5,$6,$7,$8,$9); system("ssh meta01 rbh-du -H -u " $1 "| cut -f1")}' OFS='\t' 2> /dev/null | tail -n +2 |\
+sort -t$'\t' -k8,8rh > ${output}
+sed -i '/^root/d' ${output} # to remove root user
+
+# condition for clearing the output file from blocked users
+# condition for clearing users with the "/sbin/nologin" status
+if [[ $without_blocked_users || $without_sbinnologin_users ]]; then
+	touch temp;
+	cat ${output} | while read line; do
+		username=`echo $line | awk '{print $1}'` ;
+		if ${without_blocked_users}; then
+			if [[ -n $(sudo passwd --status ${username} | grep "Password locked") ]]; then
+				echo ${username} >> temp;
+			fi
+		fi
+		if ${without_sbinnologin_users}; then
+			if [[ -n $(getent passwd ${username} | grep "/sbin/nologin") ]]; then
+				echo ${username} >> temp;
+			fi
+		fi
+	done;
+	for name in $(sort -u temp); do
+		grep -v -P "^${name}\t" ${output} > t && mv t ${output};
+	done;
+	rm temp;
+fi
+
