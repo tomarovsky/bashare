2b5a62798620bc19092a984ee5c8bd128bebcd3f
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-01-07 13:42:56 +0300

    new options added

--- pseudosomal_region.sh
+++ pseudosomal_region.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
 file=''
-scaf=''
+scafname=''
+window=''
+whole_genome_stats=''
 
 print_usage() {
 	echo "Creating files for genome vizualization." 
 	echo "'-s' sex scsffold name"
-	echo "'-i' file *_10000_windows_stats name"
+	echo "'-i' file *_windows_stats name"
+	echo "'-w' window size in *_windows_stats file"
+	echo "'-g' *_whole_genome_stats file"
+
 }
 
-while getopts 'i:s:' flag; do
+while getopts 'i:s:w:' flag; do
 	case "${flag}" in
-		s) scaf="${OPTARG}" ;;
+		s) scafname="${OPTARG}" ;;
 		i) file="${OPTARG}" ;;
+		w) window="${OPTARG}" ;;
+		g) whole_genome_stats="${OPTARG}" ;;
 		*) print_usage
 			exit 1 ;;
 	esac
 done
 
 # script
-mkdir pseudosomal_region/
-cd pseudosomal_region/
+mkdir PAR/; cd PAR/;
 
-cat ../${file} | awk '{ if ($1 == "'${scaf}'") print $0}' > ${file%_*_*_*}_chrscaf.csv
+cat ../${file} | awk '{ if ($1 == "'${scafname}'") print $0}' > ${file%_*_*_*}_chrscaf.csv
 
-python3 $TOOLS/Biocrutch/scripts/Coverage/pseudoautosomal_region.py -f 5000 -r 10 -i *_chrscaf.csv -m $(cat ../*_whole_genome_stats.csv | sed -n 2p | awk '{print $2}') -o ${file%_*_*_*} | tee ${file%_*_*_*}_pseudo.log
+python3 $TOOLS/Biocrutch/scripts/PAR/pseudoautosomal_region.py -f ${window} -i *_chrscaf.csv -s ${scafname} -m $(cat ${whole_genome_stats} | sed -n 2p | awk '{print $2}') -o ${file%_*_*_*} | tee ${file%_*_*_*}_pseudo.log
 
 cd -
