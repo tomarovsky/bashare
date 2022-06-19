764dd07f8480eb20077050e174199bd3244dc270
Tomarovsky <andrey.tomarovsky@gmail.com>
2022-06-19 22:15:37 +0300

    fixes

--- bcftools_filtration.sh
+++ bcftools_filtration.sh
@@ -1,54 +1,35 @@
 #!/bin/bash
 
-vcf=''
+BWA_ALIGNMENT="/mnt/tank/scratch/skliver/common/mustelidae/martes_martes/genome/alignment/hic.purged/bwa_alignment"
+VCF="mmar.allsamples.vcf.gz"
+PREFIX=${VCF%.*.*}
 
-print_usage() {
-	echo "Creating files for genome vizualization." 
-	echo "Usage: '-i' your VCF file"
-}
+#---- script ----
+mkdir bcftools_filtration/
+cd bcftools_filtration/
 
-while getopts 'i:' flag; do
-	case "${flag}" in
-		i) vcf="${OPTARG}" ;;
-		*) print_usage
-			exit 1 ;;
-	esac
+echo "VCF filtration"
+bcftools filter -S . -O v -o ${PREFIX}.filt.vcf --exclude 'QUAL < 20.0 || (FORMAT/SP > 60.0 | FORMAT/DP < 5.0 | FORMAT/GQ < 20.0)' ../${VCF}
+
+echo "Sample separation"
+touch samples.tmp
+for SAMPLE in `bcftools query -l ${PREFIX}.filt.vcf`; do
+	echo "Sample: ${SAMPLE}";
+	vcf-subset --exclude-ref -c ${SAMPLE} ${PREFIX}.filt.vcf > ${SAMPLE}.${PREFIX}.filt.vcf;
+	bedtools intersect -header -v -a ${SAMPLE}.${PREFIX}.filt.vcf -b ${BWA_ALIGNMENT}/${SAMPLE}/${SAMPLE}.mmar.hic.purged.mkdup.per-base.bed.gz.max250.min33 > ${SAMPLE}.${PREFIX}.filt.masked.vcf
+	echo ${SAMPLE}.${PREFIX}.filt.masked.vcf >> samples.tmp;
 done
 
-# paths
-# export PATH=${PATH}:/mnt/tank/scratch/atomarovsky/tools/bcftools-1.12/bin:/mnt/tank/scratch/atomarovsky/tools/parallel-20210622/bin:/mnt/tank/scratch/atomarovsky/tools/vcftools-0.1.16/bin
-
-# vcf prefix
-pref=${vcf%.*.*}
-
-# script
-mkdir bcftools_filtration;
-cd  bcftools_filtration/;
-
-echo "bcftool filter..."
-bcftools filter -S . -o ${pref}.filt.vcf --exclude 'QUAL < 20.0 || (FORMAT/SP > 60.0 | FORMAT/DP < 5.0 | FORMAT/GQ < 20.0)' ../${vcf}
-
-fvcf=${pref}.filt.vcf
-pref=${fvcf%.*}
-
-touch tmp.list
-echo "bcftools query..."
-for SAMPLE in `bcftools query -l ${fvcf}`; do
-	echo "Handling ${SAMPLE}..." ; 
-	vcf-subset --exclude-ref -c ${SAMPLE} ${fvcf} > ${pref}.${SAMPLE}.vcf ; 
-	echo ${pref}.${SAMPLE}.vcf >> tmp.list ;
+for FILE in $(cat samples.tmp); do
+	echo "indel and snp";
+	bcftools  filter -i  'TYPE="indel"' -O v ${FILE} > ${FILE%.*}.indel.vcf;
+	bcftools  filter -i  'TYPE="snp"' -O v ${FILE} > ${FILE%.*}.snp.vcf;
+	echo "hetero variants"
+	bcftools filter -i 'FMT/GT = "het"' -O v ${FILE%.*}.indel.vcf > ${FILE%.*}.indel.hetero.vcf;
+	bcftools filter -i 'FMT/GT = "het"' -O v ${FILE%.*}.snp.vcf > ${FILE%.*}.snp.hetero.vcf;
+	echo "homo variants"
+	bcftools filter -i 'FMT/GT = "hom"' -O v ${FILE%.*}.indel.vcf > ${FILE%.*}.indel.homo.vcf;
+	bcftools filter -i 'FMT/GT = "hom"' -O v ${file%.*}.snp.vcf > ${file%.*}.snp.homo.vcf;
 done
 
-for file in $(cat tmp.list); do
-	echo "bcftools filter...";
-	bcftools  filter -i  'TYPE="indel"' -O v ${file} > ${file%.*}.indels.vcf && bcftools  filter -i  'TYPE="snp"' -O v ${file} > ${file%.*}.SNPs.vcf;
-	echo "get hyterozygous variants..."
-	bcftools filter -i 'FMT/GT = "het"' -O v ${file%.*}.indels.vcf > ${file%.*}.indels.hetero.vcf && bcftools filter -i 'FMT/GT = "het"' -O v ${file%.*}.SNPs.vcf > ${file%.*}.SNPs.hetero.vcf
-	echo "get homozygous variants..."
-	bcftools filter -i 'FMT/GT = "hom"' -O v ${file%.*}.indels.vcf > ${file%.*}.indels.homo.vcf && bcftools filter -i 'FMT/GT = "hom"' -O v ${file%.*}.SNPs.vcf > ${file%.*}.SNPs.homo.vcf
-done
-
-rm tmp.list
-
-# Draw densities of heterozygous files
-# $TOOLS/bashare/draw_densities_of_hetero.sh
+rm samples.tmp
