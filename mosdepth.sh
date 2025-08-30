608b5b9213130b89c59e7776f3661a55d2fdd81b
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-08-30 06:15:25 +0300

    fix

--- mosdepth.sh
+++ mosdepth.sh
@@ -8,13 +8,18 @@ BAM_FILE=$1
 OUTPUT=$2
 THREADS=$3
 
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate mosdepth
+
 if [[ $# -ne 3 ]]; then
     echo "Usage: $0 BAM_FILE OUTPUT THREADS"
     exit 1
 fi
 
-source $(conda info --base)/etc/profile.d/conda.sh
-conda activate mosdepth
+if [[ ! -f "${BAM_FILE}.bai" ]]; then
+    echo "Index ${BAM_FILE}.bai not found. Creating index..."
+    samtools index "$BAM_FILE"
+fi
 
 mosdepth --mapq 20 --threads $THREADS ${OUTPUT}.mosdepth.mapq20 $BAM_FILE
 
