b39f0d73ea6364b3f24a84c3141ec5502838e3f8
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-09-15 09:25:05 +0300

    fix

--- assembly_stats.sh
+++ assembly_stats.sh
@@ -16,7 +16,7 @@ if [[ "$FASTA" == *.gz ]]; then
 fi
 
 if [[ ! -f "${FASTA}.fai" ]]; then
-    samtools faidx "$FASTA"
+    conda run -n varcall samtools faidx "$FASTA"
 fi
 
 FASTA_PREFIX=${FASTA%.*}
