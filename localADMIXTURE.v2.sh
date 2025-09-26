b79d67bb6ce1b596df81eb5e64d77205f75a41b8
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-09-26 15:42:00 +0300

    fix

--- localADMIXTURE.v2.sh
+++ localADMIXTURE.v2.sh
@@ -27,22 +27,19 @@ for CHR in $SCAFFOLDS; do
         bcftools view --threads 1 --regions ${REGION} -O v -o ${CHR}.${REGION}.vcf ${VCF};
         conda deactivate && conda activate plink1.9;
         if plink --vcf ${CHR}.${REGION}.vcf --out ${CHR}.${REGION}.plink --make-bed --threads 1; then
-            # cat ${CHR}.${REGION}.plink.bim | awk -F '_' '{print $3"_"$4"_"$5}' > ${CHR}.${REGION}.plink.bim.tmp;
-            # mv ${CHR}.${REGION}.plink.bim ${CHR}.${REGION}.plink.bim.raw;
-            # mv ${CHR}.${REGION}.plink.bim.tmp ${CHR}.${REGION}.plink.bim;
             conda deactivate && conda activate admixture;
             admixture -j1 --cv ${CHR}.${REGION}.plink.bed 2 | tee ${CHR}.${REGION}.admixture.log;
             for SAMPLE in {1..33}; do
                 ADMIXTURE_OUTLINE=$(sed "${SAMPLE}q;d" ${CHR}.${REGION}.plink.2.Q | awk '{print $1"\t"$2}');
                 echo $ADMIXTURE_OUTLINE;
-                echo -e "${REG}\t${ADMIXTURE_OUTLINE}" >> ${SAMPLE}.${CHR}.admixture.concat.Q;
+                echo -e "HiC_scafflod_${REG}\t${ADMIXTURE_OUTLINE}" >> ${SAMPLE}.${CHR}.admixture.concat.Q;
             done;
             cat ${CHR}.${REGION}.admixture.log >> ${SAMPLE}.${CHR}.admixture.concat.log;
             rm *plink* *.vcf *.admixture.log;
         else
             echo 'WARNING! Empty PLINK results!';
             for SAMPLE in {1..33}; do
-                echo -e "${REG}\tGRAY\tGRAY" >> ${SAMPLE}.${CHR}.admixture.concat.Q;
+                echo -e "HiC_scafflod_${REG}\tEMPTY\tEMPTY" >> ${SAMPLE}.${CHR}.admixture.concat.Q;
             done;
             rm *plink* *.vcf;
         conda deactivate;
