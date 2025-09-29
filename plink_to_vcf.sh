59a78efa01112cfb69014a149b1e49de242e2b0d
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-09-29 12:10:06 +0300

    fix

--- plink_to_vcf.sh
+++ plink_to_vcf.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # Usage:
-# $TOOLS/bashare/plink_to_vcf.sh PLINK_PREFIX OUTPREFIX
+# $TOOLS/bashare/plink_to_vcf.sh PLINK_PREFIX
 
 PLINK_PREFIX=$1
-OUTPREFIX=$2
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate plink1.9
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 BFILE OUTPREFIX"
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 PLINK_PREFIX"
     exit 1
 fi
 
 # plink -> vcf.gz
-plink --bfile $PLINK_PREFIX --recode vcf bgz --out $OUTPREFIX
+plink --bfile $PLINK_PREFIX --recode vcf bgz --out $PLINK_PREFIX
 
 # index
 conda activate varcall
-bcftools index ${OUTPREFIX}.vcf.gz
+bcftools index $PLINK_PREFIX.vcf.gz
 
