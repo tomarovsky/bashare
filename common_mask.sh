a8ce355c058429831cbda6c940858eec20b5bdcc
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-04 07:25:10 +0300

    fix

--- common_mask.sh
+++ common_mask.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # common_mask.sh BED_FILES OUTPREFIX THREADS
 
-BED_FILES=$1
-OUTPREFIX=$2
-THREADS=$3
-
-if [[ $# -ne 3 ]]; then
-    echo "Usage: $0 BED_FILES OUTPREFIX THREADS"
+if [[ $# -lt 3 ]]; then
+    echo "Usage: $0 BED_FILE1 [BED_FILE2 ...] OUTPREFIX THREADS"
     exit 1
 fi
 
+ARGS=("$@")
+OUTPREFIX="${ARGS[-2]}"
+THREADS="${ARGS[-1]}"
+BED_FILES=("${ARGS[@]:0:${#ARGS[@]}-2}")
+
 # sort all input BED files
-echo "$BED_FILES" | tr ' ' '\n' | parallel -j "$THREADS" '
-    echo 'Sorting {}'
+printf "%s\n" "${BED_FILES[@]}" | parallel -j "$THREADS" '
+    echo "Sorting {}"
     sort -k1,1 -k2,2n {} > {}.sorted.bed
 '
 
 # array of sorted BED files
 sorted_bed_files=()
-for bed in $BED_FILES; do
+for bed in "${BED_FILES[@]}"; do
     sorted_bed_files+=("${bed}.sorted.bed")
 done
 
@@ -30,21 +31,21 @@ for i in "${!sorted_bed_files[@]}"; do
     for j in "${sorted_bed_files[@]:$((i+1))}"; do
         echo "${sorted_bed_files[i]} $j"
     done
-done | parallel -j "$THREADS" '
+done | parallel -j "$THREADS" --colsep ' ' '
     a={1}
     b={2}
     a_sample=$(basename "${a}" | cut -d"." -f1)
     b_sample=$(basename "${b}" | cut -d"." -f1)
     tmpfile="'"$tmpdir"'/${a_sample}_${b_sample}.intersect"
-    echo \"Processing \$a vs \$b -> \$tmpfile\"
+    echo "Processing $a vs $b -> $tmpfile"
     bedtools intersect \
         -a "${a}" \
         -b "${b}" \
         -sorted > "$tmpfile"
 '
+
 # merge all intersection results and create final BED file
-bedtools merge -i <(cat "$tmpdir"/*.intersect | sort -k1,1 -k2,2n) > ${OUTPREFIX}.merge_all.intersect_2.mapq10.bed
+bedtools merge -i <(cat "$tmpdir"/*.intersect | sort -k1,1 -k2,2n) > "${OUTPREFIX}.merge_all.intersect_2.mapq10.bed"
 
-# rm -r "$tmpdir"
-
-
+# Optional: cleanup temporary files
+rm -r "$tmpdir"
