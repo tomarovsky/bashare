25af0672e99f7ead19aff1dd829f8919cc2b8049
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-04 20:10:46 +0300

    fix env

--- mask_from_coverage.sh
+++ mask_from_coverage.sh
@@ -18,6 +18,7 @@ HEMI_REGIONS_BED=$4 # BED with hemizygous regions
 SAMPLE=$(basename "$PER_BASE_BED_FILE" | cut -d. -f1)
 
 source $(conda info --base)/etc/profile.d/conda.sh
+conda activate py38
 
 if grep -qw "$SAMPLE" "$MALES"; then
     echo "[INFO] $SAMPLE == MALE"
@@ -25,12 +26,9 @@ if grep -qw "$SAMPLE" "$MALES"; then
     HEMI_TMP="${SAMPLE}.hemi.tmp.per-base.bed.gz"
     DIP_TMP="${SAMPLE}.diploid.tmp.per-base.bed.gz"
 
-    conda activate varcall
     bedtools intersect -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "$HEMI_TMP"
     bedtools intersect -v -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "$DIP_TMP"
-    conda deactivate
 
-    conda activate py38
     "$TOOLS/MAVR/scripts/alignment/coverage/generate_mask_from_coverage_bed.py" \
         -c ${SAMPLE}.diploid.tmp.per-base.bed.gz -m "$COVERAGE" \
         --max_coverage_threshold 2.5 --min_coverage_threshold 0.33 \
@@ -41,8 +39,8 @@ if grep -qw "$SAMPLE" "$MALES"; then
         --max_coverage_threshold 2.5 --min_coverage_threshold 0.33 \
         -o ${SAMPLE}.hemi.per-base.mask.bed
 
-    cat ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed | sort -k1,1 -k2,2n > "${PER_BASE_BED_FILE%.*.*}.max250.min33.bed"
-    # rm -f ${SAMPLE}.diploid.tmp.per-base.bed.gz ${SAMPLE}.hemi.tmp.per-base.bed.gz ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed
+    cat ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed | sort -S 20G -k1,1 -k2,2n > "${PER_BASE_BED_FILE%.*.*}.max250.min33.bed"
+    rm ${SAMPLE}.diploid.tmp.per-base.bed.gz ${SAMPLE}.hemi.tmp.per-base.bed.gz ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed
 
 else
     echo "[INFO] $SAMPLE == FEMALE"
