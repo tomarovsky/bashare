00b93c0a12b5ad9a736e43509215fbb8346dcbcb
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-11 17:01:51 +0300

    fix

--- admixture/admixtools.D.sh
+++ admixture/admixtools.D.sh
@@ -50,7 +50,7 @@ add_group "$HYBRID" "hybrid"
 add_group "$OUTGROUP" "outgroup"
 
 # --- Генерация poplist.txt ---
-echo "mzib   mmar   hybrid   outgroup" > "$POPFILE"
+echo -e "mzib\tmmar\thybrid\toutgroup" > "$POPFILE"
 
 # --- Конфигурация qpDstat ---
 PARFILE="${TMPDIR}/qpDstat.par"
