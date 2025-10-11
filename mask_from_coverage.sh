99bfb804e1c9c796fc23ecd60da89c40c4000692
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-11 09:13:51 +0300

    Update mask_from_coverage.sh

--- mask_from_coverage.sh
+++ mask_from_coverage.sh
@@ -23,21 +23,21 @@ conda activate py38
 if grep -qw "$SAMPLE" "$MALES"; then
     echo "[INFO] $SAMPLE == MALE"
 
-    HEMI_TMP="${SAMPLE}.hemi.tmp.per-base.bed.gz"
-    DIP_TMP="${SAMPLE}.diploid.tmp.per-base.bed.gz"
+    bedtools intersect -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "${SAMPLE}.hemi.tmp.per-base.bed.gz" &
+    bedtools intersect -v -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "${SAMPLE}.diploid.tmp.per-base.bed.gz" &
 
-    bedtools intersect -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "$HEMI_TMP"
-    bedtools intersect -v -a <(zcat "$PER_BASE_BED_FILE") -b "$HEMI_REGIONS_BED" | gzip > "$DIP_TMP"
+    wait
 
     "$TOOLS/MAVR/scripts/alignment/coverage/generate_mask_from_coverage_bed.py" \
         -c ${SAMPLE}.diploid.tmp.per-base.bed.gz -m "$COVERAGE" \
         --max_coverage_threshold 2.5 --min_coverage_threshold 0.33 \
-        -o ${SAMPLE}.diploid.per-base.mask.bed
-
+        -o ${SAMPLE}.diploid.per-base.mask.bed &
     "$TOOLS/MAVR/scripts/alignment/coverage/generate_mask_from_coverage_bed.py" \
         -c ${SAMPLE}.hemi.tmp.per-base.bed.gz -m "$(awk -v cov=$COVERAGE 'BEGIN{print cov/2}')" \
         --max_coverage_threshold 2.5 --min_coverage_threshold 0.33 \
-        -o ${SAMPLE}.hemi.per-base.mask.bed
+        -o ${SAMPLE}.hemi.per-base.mask.bed &
+
+    wait
 
     cat ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed | sort -S 20G -k1,1 -k2,2n > "${PER_BASE_BED_FILE%.*.*}.max250.min33.bed"
     rm ${SAMPLE}.diploid.tmp.per-base.bed.gz ${SAMPLE}.hemi.tmp.per-base.bed.gz ${SAMPLE}.diploid.per-base.mask.bed ${SAMPLE}.hemi.per-base.mask.bed
