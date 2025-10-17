38f8cb4590637afbb3709182e4aef64f788ccbb3
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-17 11:50:23 +0300

    Update admixtools.summary.sh

--- admixture/admixtools.summary.sh
+++ admixture/admixtools.summary.sh
@@ -10,8 +10,8 @@ fi
 echo -e "Source_1\tSource_2\tTarget\tOutgroup\tStat\tZ-score\tABBA\tBABA\tSNPs"
 
 for file in "$@"; do
-    grep "^result:" "$file" | while read -r line; do
-        line_clean=$(echo "$line" | sed 's/^result:[[:space:]]*//')
+    grep "result:" "$file" | while read -r line; do
+        line_clean=$(echo "$line" | sed 's/result:[[:space:]]*//')
         echo -e "$(echo "$line_clean" | awk '{$1=$1; print}' | tr -s ' ' '\t')"
     done
 done
