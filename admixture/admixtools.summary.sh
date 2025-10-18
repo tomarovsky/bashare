cfa9838761550150bcc4b24198daaba983625e6c
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-18 17:32:38 +0300

    Update admixtools.summary.sh

--- admixture/admixtools.summary.sh
+++ admixture/admixtools.summary.sh
@@ -8,7 +8,8 @@ if [[ $# -lt 1 ]]; then
 fi
 
 # Header
-echo -e "Source_1\tSource_2\tTarget\tOutgroup\tStat\tZ-score\tABBA\tBABA\tSNPs"
+echo -e "F3:Source_1\tSource_2\tTarget\tF3\tstderr\tZ-score\tSNPs"
+echo -e "D/F4:Target\tSource_1\tSource_2\tOutgroup\tD/F4\tZ-score\tABBA\tBABA\tSNPs"
 
 for file in "$@"; do
     grep "result:" "$file" | while read -r line; do
