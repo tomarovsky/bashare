2161d8acd1f8c8167719c78953d7e33d8aa6f8c6
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-23 13:20:36 +0300

    Update number_of_SNPs.sh

--- number_of_SNPs.sh
+++ number_of_SNPs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ "$#" -lt 3 ]; then
-    echo "Usage: $0 VCF_FILE MASK_BED THRESHOLD EXCLUDE_SCAFFOLD1[,SCAFFOLD2,...]"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 VCF_FILE EXCLUDE_SCAFFOLDS[,SCAFFOLD2,...]"
     exit 1
 fi
 
@@ -10,17 +10,10 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate varcall
 
 VCF_FILE=$1
-MASK_BED=$2
-THRESHOLD=$3
-EXCLUDE_SC=$4
+EXCLUDE_SC=$2
 
 # grep regex
 EXCLUDE_REGEX=$(echo "$EXCLUDE_SC" | sed 's/,/|/g')
 
 # Number of SNPs
-zcat "$VCF_FILE" | \
-grep -v '^#' | \
-grep -Pv "^($EXCLUDE_REGEX)\t" | \
-awk '{print $1"\t"$2-1"\t"$2}' | \
-bedtools intersect -v -a - -b <(awk '$4>'"$THRESHOLD"'' "$MASK_BED") | \
-wc -l
+zcat "$VCF_FILE" | grep -v '^#' | grep -Pvc "^($EXCLUDE_REGEX)\t"
