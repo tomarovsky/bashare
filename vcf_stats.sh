48c3f66fc7238b358bd614d7fd07140a01c2c542
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-24 09:40:03 +0300

    Create vcf_stats.sh

---
+++ vcf_stats.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 VCF_FILE TRACK_FILE EXCLUDE_SCAFFOLD EXCLUDE_TRACK_CHR" >&2
+    exit 1
+fi
+
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate py38
+
+VCF_FILE=$1
+TRACK_FILE=$2
+EXCLUDE_SCAFFOLD=$3
+EXCLUDE_TRACK_CHR=$4
+
+NUMBER_OF_SNPS=$(zcat "${VCF_FILE}" | grep -v '^#' | grep -v "^${EXCLUDE_SCAFFOLD}\t" | wc -l)
+
+MEAN_MEDIAN=$(python3 - <<END
+import pandas as pd
+import sys
+
+track_file = "$TRACK_FILE"
+exclude_chr = "$EXCLUDE_TRACK_CHR"
+
+df = pd.read_csv(track_file, sep="\t", header=0)
+sample_id = df.columns[4]
+
+# masking
+df_filtered = df[(df['scaffold'] != exclude_chr) & (df['color'] != 'gray')].copy()
+
+df_filtered[sample_id] = pd.to_numeric(df_filtered[sample_id], errors='coerce')
+
+mean = df_filtered[sample_id].mean()
+median = df_filtered[sample_id].median()
+
+print(f"{mean}\t{median}")
+END
+)
+
+echo -e "${NUMBER_OF_SNPS}\t${MEAN_MEDIAN}"
