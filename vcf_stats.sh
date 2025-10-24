c5f35b30983cd532dd98576f5067896dc18f8e43
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-24 10:03:44 +0300

    Update vcf_stats.sh

--- vcf_stats.sh
+++ vcf_stats.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 VCF_FILE TRACK_FILE EXCLUDE_SCAFFOLD EXCLUDE_TRACK_CHR" >&2
+    echo "Usage: $0 VCF_FILE TRACK_FILE WHITELIST_FILE EXCLUDE_TRACK_CHR"
     exit 1
 fi
 
@@ -11,10 +11,10 @@ conda activate py38
 
 VCF_FILE=$1
 TRACK_FILE=$2
-EXCLUDE_SCAFFOLD=$3
+WHITELIST_FILE=$3
 EXCLUDE_TRACK_CHR=$4
 
-NUMBER_OF_SNPS=$(zcat "${VCF_FILE}" | grep -v '^#' | grep -v "^${EXCLUDE_SCAFFOLD}\t" | wc -l)
+NUMBER_OF_SNPS=$(zcat "${VCF_FILE}" | grep -v '^#' | grep -w -f "$WHITELIST_FILE" | wc -l)
 
 MEAN_MEDIAN=$(python3 - <<END
 import pandas as pd
@@ -26,13 +26,12 @@ exclude_chr = "$EXCLUDE_TRACK_CHR"
 df = pd.read_csv(track_file, sep="\t", header=0)
 sample_id = df.columns[4]
 
-# masking
 df_filtered = df[(df['scaffold'] != exclude_chr) & (df['color'] != 'gray')].copy()
 
-mean = round(df_filtered[sample_id].mean(), 2)
-median = round(df_filtered[sample_id].median(), 2)
+mean_val = round(df_filtered[sample_id].mean(), 2)
+median_val = round(df_filtered[sample_id].median(), 2)
 
-print(f"{mean}\t{median}")
+print(f"{mean_val}\t{median_val}")
 END
 )
 
