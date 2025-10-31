91dea2a698e6bc6a74348af5febaabd07d0a0ef0
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-31 14:45:51 +0300

    Update bed_with_windows.sh

--- bed_with_windows.sh
+++ bed_with_windows.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
 if [ "$#" -lt 3 ] || [ "$#" -gt 5 ]; then
-    echo "Usage: $0 LEN_FILE WHITELIST_FILE OUTPUT_FILE WINDOW_SIZE STEP_SIZE"
+    echo "Usage: $0 LEN_FILE WHITELIST_FILE OUTPUT_FILE [WINDOW_SIZE] [STEP_SIZE]" >&2
+    echo "  [WINDOW_SIZE] default: 1000000" >&2
+    echo "  [STEP_SIZE] default: 100000" >&2
     exit 1
 fi
 
 LEN_FILE="$1"
 WHITELIST_FILE="$2"
 OUTPUT_FILE="$3"
-WINDOW_SIZE="${4:-1000000}"
-STEP_SIZE="${5:-100000}"
-
-# Clear old output file
-> "$OUTPUT_FILE"
+WINDOW_SIZE=${4:-1000000}
+STEP_SIZE=${5:-100000}
 
 {
-while read -r scaffold_name length_from_len_file; do
-    length_line=$(grep -Pw "^${scaffold_name}\t" "$LEN_FILE")
+    while read -r scaffold_name; do
+        length_line=$(grep -Pw "^${scaffold_name}\t" "$LEN_FILE")
 
-    if [ -n "$length_line" ]; then
-        scaffold_length=$(echo "$length_line" | awk '{print $2}')
+        if [ -n "$length_line" ]; then
+            scaffold_length=$(echo "$length_line" | awk '{print $2}')
 
-        # Generate windows
-        start_coord=0
-        while [ "$start_coord" -lt "$scaffold_length" ]; do
-            stop_coord=$((start_coord + WINDOW_SIZE))
-
-            if [ "$stop_coord" -gt "$scaffold_length" ]; then
-                break
+            if [ "$scaffold_length" -lt "$WINDOW_SIZE" ]; then
+                echo "warning: scaffold $scaffold_name (${scaffold_length}) less than (${WINDOW_SIZE}). skipped." >&2
+                continue
             fi
 
-            printf "%s\t%d\t%d\n" "$scaffold_name" "$start_coord" "$stop_coord"
+            # generate windows
+            start_coord=0
+            while [ "$start_coord" -lt "$scaffold_length" ]; do
+                stop_coord=$((start_coord + WINDOW_SIZE))
 
-            start_coord=$((start_coord + STEP_SIZE))
-        done
-    fi
+                if [ "$stop_coord" -gt "$scaffold_length" ]; then
+                    break
+                fi
 
-done < "$WHITELIST_FILE"
+                echo -e "${scaffold_name}\t${start_coord}\t${stop_coord}"
+
+                start_coord=$((start_coord + STEP_SIZE))
+            done
+
+        else
+            echo "warning: '$scaffold_name' not found in '$LEN_FILE'" >&2
+        fi
+
+    done < "$WHITELIST_FILE"
+
 } > "$OUTPUT_FILE"
