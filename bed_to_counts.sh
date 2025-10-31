d27a38bbfaf353b033e352f9e880403d90eca458
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-10-31 14:15:52 +0300

    Update bed_to_counts.sh

--- bed_to_counts.sh
+++ bed_to_counts.sh
@@ -8,20 +8,18 @@ if [[ $# -ne 2 ]]; then
 fi
 
 MASK_BED=$1
-WINDOW_BED=$2 # e.g., w1mb.s100kb.features.bed
+WINDOW_BED=$2 # w1mb.s100kb.bed
 
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate varcall
 
 # Step 1: bedtools intersect
-INTERSECT_FILE="${MASK_BED%.*}.w1mb.s100kb.intersect_wao.bed"
-bedtools intersect -a "$WINDOW_BED" -b "$MASK_BED" -wao > "$INTERSECT_FILE"
+bedtools intersect -a "$WINDOW_BED" -b "$MASK_BED" -wao > "${MASK_BED%.*}.intersect_wao.bed"
 
 # Step 2: Grouping and counting
 conda deactivate && conda activate py38
-OUTPUT_FILE="${MASK_BED%.*}.w1mb.s100kb.counts.bed"
 
-python3 - "$INTERSECT_FILE" "$OUTPUT_FILE" <<EOF
+python3 - "$INTERSECT_FILE" "${MASK_BED%.*}.counts.bed" <<EOF
 import pandas as pd
 import sys
 
@@ -32,7 +30,6 @@ df = pd.read_csv(input_file, sep='\t', header=None)
 df.columns = ['scaffold1', 'start', 'end', 'scaffold2', 'start2', 'end2', 'value']
 result = df.groupby(['scaffold1', 'start', 'end'])['value'].sum().reset_index()
 result.to_csv(output_file, sep='\t', index=False, header=False)
-
 EOF
 
 rm "$INTERSECT_FILE"
