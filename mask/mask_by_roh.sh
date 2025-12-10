679e461eec3fef92d542fcee9a797d1eaf43c486
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-12-10 17:09:58 +0300

    Update mask_by_roh.sh

--- mask/mask_by_roh.sh
+++ mask/mask_by_roh.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Usage:
-# $TOOLS/bashare/mask_by_roh.sh FEATURES_ROH SYN_FILE
-# parallel -v --progress -j 8 "$TOOLS/bashare/mask_by_roh.sh {} genome.syn" ::: *.w100kb.s10kb.features.roh
+set -euo pipefail
+
+source "$TOOLS/bashare/lib/log_functions.sh"
 
 FEATURES_ROH=$1
 SYN_FILE=$2
 
 if [[ $# -ne 2 ]]; then
     echo "Usage: $0 FEATURES_ROH SYN_FILE"
+    echo 'Example: parallel -v --progress -j 8 "$TOOLS/bashare/mask/mask_by_roh.sh {} genome.syn" ::: *.w100kb.s10kb.features.roh'
     exit 1
 fi
 
 awk -F "\t" 'NR==FNR {map[$2]=$1; next} {if ($1 in map) $1=map[$1]; print}' OFS="\t" ${SYN_FILE} ${FEATURES_ROH} > ${FEATURES_ROH}.mask.bed
+
+log_info "Done!"
