2f1be2ad95da8e48c89149a381da2156212a20ed
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-12-14 12:31:05 +0300

    del old files

--- old/makarich_scripts/funlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find . -xtype l 2> tmp0.txt
-cat tmp0.txt | awk '{print $2}' | sed 's/`*//g' | sed 's/..$//' >> tmp1.txt
-for i in $(cat tmp1.txt); do unlink $i; done
-rm tmp0.txt tmp1.txt
