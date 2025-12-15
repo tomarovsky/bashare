3807422533423199e88c8cd230e5b35a69a3f5a9
Tomarovsky <andrey.tomarovsky@gmail.com>
2025-12-15 21:27:18 +0300

    Update bed_masked_persent.sh

--- bed/bed_masked_persent.sh
+++ bed/bed_masked_persent.sh
@@ -1,46 +1,33 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ $# -lt 2 || $# -gt 3 ]]; then
-    echo "Calculate masked percent of genome by BED regions"
-    echo "Usage: $0 mask.bed genome.len [comma separated scaffolds for exclusion]"
-    echo "Example scaffolds for exclusion: scaffold_19,chrX"
+if [[ $# -lt 2 ]]; then
+    echo "Calculate genome coverage by BED"
+    echo "Usage: $0 input.bed genome.len [exclude_scaffold1 exclude_scaffold2 ...]"
     exit 1
 fi
 
-BED="$1"
-LEN="$2"
-EXCLUDE="${3:-}"
+BED_FILE="$1"
+LEN_FILE="$2"
+shift 2
+EXCLUDE_LIST=("$@")
 
-# Filter by scaffolds (if a list is provided)
-if [[ -n "$EXCLUDE" ]]; then
-    EXCL_AWK='
-        BEGIN {
-            n = split(excl, a, ",")
-            for (i = 1; i <= n; i++) excl_map[a[i]] = 1
-        }
-        !($1 in excl_map)
-    '
-else
-    EXCL_AWK='{print}'
-fi
+filter_scaffolds() {
+    if [ ${#EXCLUDE_LIST[@]} -eq 0 ]; then
+        cat
+    else
+        grep -v -F -w -f <(printf "%s\n" "${EXCLUDE_LIST[@]}")
+    fi
+}
 
-# Total length of masked regions
-TOTAL_MASKED=$(
-    awk -v excl="$EXCLUDE" "$EXCL_AWK" "$BED" \
-    | sort -k1,1 -k2,2n \
-    | bedtools merge -i - \
-    | awk '{sum += $3 - $2} END {print sum}'
-)
+GENOME_LEN=$(filter_scaffolds < "$LEN_FILE" | awk '{sum += $2} END {print sum+0}')
 
-# Total length of genome
-GENOME_LENGTH=$(
-    awk -v excl="$EXCLUDE" "$EXCL_AWK" "$LEN" \
-    | awk '{sum += $2} END {print sum}'
-)
+REGIONS_LEN=$(filter_scaffolds < "$BED_FILE" | \
+              sort -k1,1 -k2,2n | \
+              bedtools merge -i - | \
+              awk '{sum += $3 - $2} END {print sum}')
 
-# Percentage
-MASKED_PERCENT=$(awk -v bed="$TOTAL_MASKED" -v genome="$GENOME_LENGTH" \
-    'BEGIN { printf "%.2f", (bed / genome) * 100 }')
+MASKED_PERCENT=$(awk -v total="$GENOME_LEN" -v covered="$REGIONS_LEN" \
+    'BEGIN { printf "%.2f%%\n", (covered/total)*100 }')
 
-echo "${GENOME_LENGTH}\t${TOTAL_MASKED}\t${MASKED_PERCENT}"
+echo -e "$GENOME_LEN\t$REGIONS_LEN\t$MASKED_PERCENT"
